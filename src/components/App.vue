274d0471e93e90b7bab1aa47f938b07bc60af163
Ania Brajtkopf <aniabrajtkopf@gmail.com>
2017-09-19 14:20:08 +0200

    initial commit

---
+++ src/components/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    {{ title }}
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'app',
+    data() {
+      return {
+        title: 'Random Quote Machine'
+      }
+    }
+  }
+</script>
